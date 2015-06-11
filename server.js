ab8ff146e82b565fb60a0a8b2c0bb6142e868a91
Reece <lrv378@gmail.com>
2015-06-10 22:34:46 -0600

    Add dependencies

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http')
+
+var statusCodeVar;
+function requestTime() {
+	var start = new Date();
+	http.get("http://www.google.com/index.html", function(res) {
+		//console.log('Request took:', new Date() - start, 'ms');
+	});
+}
+
+function statusCodeFunc() {
+		http.get("http://www.google.com/index.html", function(res) {
+  	statusCodeVar = res.statusCode;
+  	//console.log("Got response: " + res.statusCode);
+	}).on('error', function(e) {
+  	console.log("Got error: " + e.message);
+	});
+}
+
+var handleRequest = function (request, response) {
+	response.writeHead(200, { 'content-type': 'text/plain'});
+	response.write("Test");
+	response.end();
+};
+
+setInterval(statusCodeFunc, 1000);
+setInterval(requestTime, 1000);
+
+var server = http.createServer(handleRequest);
+server.listen(1337);
+console.log("Listening on port 1337");
