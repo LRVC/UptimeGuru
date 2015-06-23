e38f22dea53ee20f99c939c9f8fdbde889c2eb08
Reece <lrv378@gmail.com>
2015-06-23 11:50:40 -0600

    Add https endpoint

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('https://localhost:3000');
 socket.on('response', renderOrUpdateResponse);
 
 var ResponseTime = React.createClass({
