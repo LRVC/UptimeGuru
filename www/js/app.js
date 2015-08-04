858326c32093612a0b3f577888e27a4c351e1b2c
Reece <lrv378@gmail.com>
2015-08-04 11:32:25 -0600

    Add Pure layout

--- www/js/app.js
+++ www/js/app.js
@@ -12,196 +12,198 @@ var ComicNinja = React.createClass({
 		var statusCode = this.state.statusCode || '-';
 		var url = this.state.url || '-';
     return (
-      <div className="col-1-3">
-				<h2><a href={url}><strong>Digital Comic Shopper</strong></a></h2>
-					<p> Response Time: {responseTime} ms</p>
-					<p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-			</div>
-    );
-  }
-});
-
-var LearnersGuild = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseTwo', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-		var statusCode = this.state.statusCode || '-';
-		var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-				<h2 id="learnersGuild"><a href={url}><strong>The Learners Guild</strong></a></h2>
-					<p> Response Time: {responseTime} ms</p>
-					<p id={(statusCode === 200) ? 'okay' : 'fail'}> HTTP Status Code:{statusCode} </p>
-			</div>
-    );
-  }
-});
-
-var TherapyFixx = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseThree', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="therapyFixx"><a href={url}><strong>TherapyFixx</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+   //    <div className="col-1-3">
+			// 	<h2><a href={url}><strong>Digital Comic Shopper</strong></a></h2>
+			// 		<p> Response Time: {responseTime} ms</p>
+			// 		<p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+			// </div>
+      
+      <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+        <h3 class="content-subhead">
+            <i class="fa fa-rocket"></i>
+            <a href={url}><strong>Digital Comic Shopper</strong></a>
+        </h3>
+        <p> Response Time: {responseTime} ms</p>
+        <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
       </div>
     );
   }
 });
 
-var QuizRacer = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseFour', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="quizRacer"><a href={url}><strong>Quiz Racer</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var LearnersGuild = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseTwo', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+// 		var statusCode = this.state.statusCode || '-';
+// 		var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+// 				<h2 id="learnersGuild"><a href={url}><strong>The Learners Guild</strong></a></h2>
+// 					<p> Response Time: {responseTime} ms</p>
+// 					<p id={(statusCode === 200) ? 'okay' : 'fail'}> HTTP Status Code:{statusCode} </p>
+// 			</div>
+//     );
+//   }
+// });
 
-var BriskLyfe = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseFive', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="briskLyfe"><a href={url}><strong>BRISK LYFE</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var TherapyFixx = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseThree', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="therapyFixx"><a href={url}><strong>TherapyFixx</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
 
-var JournalWithClarity = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseSix', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="journalWithClarity"><a href={url}><strong>Journal With Clarity</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var QuizRacer = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseFour', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="quizRacer"><a href={url}><strong>Quiz Racer</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
 
-var RentalRater = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseSeven', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="rentalRater"><a href={url}><strong>Rental Rater</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var BriskLyfe = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseFive', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="briskLyfe"><a href={url}><strong>BRISK LYFE</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
 
-var WordsWithCharles = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseEight', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="wordsWithCharles"><a href={url}><strong>Words With Charles</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var JournalWithClarity = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseSix', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="journalWithClarity"><a href={url}><strong>Journal With Clarity</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
 
-var Belp = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    socket.on('responseNine', this.setState.bind(this));
-  },
-  render: function() {
-    var responseTime = this.state.responseTime || '-';
-    var statusCode = this.state.statusCode || '-';
-    var url = this.state.url || '-';
-    return (
-      <div className="col-1-3">
-        <h2 id="belp"><a href={url}><strong>BELP</strong></a></h2>
-          <p> Response Time: {responseTime} ms</p>
-          <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
-      </div>
-    );
-  }
-});
+// var RentalRater = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseSeven', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="rentalRater"><a href={url}><strong>Rental Rater</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
+
+// var WordsWithCharles = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseEight', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="wordsWithCharles"><a href={url}><strong>Words With Charles</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
+
+// var Belp = React.createClass({
+//   getInitialState: function() {
+//     return {};
+//   },
+//   componentDidMount: function() {
+//     socket.on('responseNine', this.setState.bind(this));
+//   },
+//   render: function() {
+//     var responseTime = this.state.responseTime || '-';
+//     var statusCode = this.state.statusCode || '-';
+//     var url = this.state.url || '-';
+//     return (
+//       <div className="col-1-3">
+//         <h2 id="belp"><a href={url}><strong>BELP</strong></a></h2>
+//           <p> Response Time: {responseTime} ms</p>
+//           <p id={(statusCode === 200) ? 'okay' : 'fail'}>HTTP Status Code: {statusCode} </p>
+//       </div>
+//     );
+//   }
+// });
 
 var MyComponent = React.createClass({
 	render: function() {
 		return (
 		<div>
 		  <ComicNinja />
-		  <LearnersGuild />
-      <TherapyFixx />
-      <QuizRacer />
-      <BriskLyfe />
-      <JournalWithClarity />
-      <RentalRater />
-      <WordsWithCharles />
-      <Belp />
+		  
 		</div>
 		)
 	}
